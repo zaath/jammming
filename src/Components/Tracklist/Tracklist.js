af26d7e9ebd2eccb55151d3267ae945bf768db28
Zaath <zebigmatt@hotmail.com>
2022-02-23 22:40:21 +0100

    Edit Tracklist Component

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Tracklist extends React.Component {
+    render() {
+        return(
+            <div className="TrackList">
+                You will add a map method that renders a set of Track components
+            </div>
+        )
+    }
+}
