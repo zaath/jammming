7e75a58480d35929fbe1761b472268af57b967ae
Zaath <zebigmatt@hotmail.com>
2022-02-26 16:26:52 +0100

    Add search term methods

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,22 @@ import './SearchBar.css';
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+    component(props) {
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+    handleTermChange(event) {
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" />
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         )
